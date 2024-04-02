2afccf4cd07c98242044ee198f8eb0caf1642d5d
Bnaya <bnaya.peretz@backslash.security>
2024-04-02 09:08:30 +0300

    WIP

--- spike.mjs
+++ spike.mjs
@@ -1,7 +1,5 @@
 // @ts-check
 
-// import { LockfileResolver, Project } from "@yarnpkg/core";
-// import { PortablePath, VirtualFS } from "@yarnpkg/fslib";
 import { default as Arborist } from "@npmcli/arborist";
 import { setTimeout } from "node:timers/promises";
 import { join } from "node:path";
@@ -10,17 +8,8 @@ import { fileURLToPath } from "node:url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
-// PortablePath
-
-// toPortablePath()
-
-// const proj = new Project(resolve('.'));
-
-// const res = new Resolver();
-
 globalThis.arb = new Arborist({
-  // path: '/Users/bnaya/dev/__DIR__/platform',
-  path: join(__dirname, 'yarn-4-project'),
+  path: join(__dirname, 'npm-10-project'),
   '@foo:registry': 'https://registry.foo.com/',
 });
 
